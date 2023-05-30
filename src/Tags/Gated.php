7b7438561858527835961b0d317e4a1affe7b95d
Christian <christianpavilonis@outlook.com>
2023-05-30 09:31:25 -0500

    Add to cart and remove from cart buttons will now render html forms instead of buttons.

--- src/Tags/Gated.php
+++ src/Tags/Gated.php
@@ -12,24 +12,22 @@ use State\Gated\Http\Controllers\Payment\SuccessfulPayment;
 class Gated extends Tags
 {
 
-    public function head() : string
+    public function head(): string
     {
-        $csrfToken      = csrf_token();
         $publishableKey = config('gated.payments.stripe.publishable_key');
 
         return <<<HEAD
-            <meta name="csrf_token" content="{$csrfToken}"> 
             <meta name="stripe_publishable_key" content="{$publishableKey}">
             <script src="https://js.stripe.com/v3/"></script>
         HEAD;
     }
 
-    public function styles() : string
+    public function styles(): string
     {
         return '<link rel="stylesheet" href="/vendor/state/gated/css/checkout.css">';
     }
 
-    public function scripts() : string
+    public function scripts(): string
     {
         return '<script src="/vendor/state/gated/js/checkout.js"></script>';
     }
@@ -42,40 +40,51 @@ class Gated extends Tags
         ];
     }
 
-    public function addToCart() : string
+    public function addToCart(): string
     {
-        return <<<ADD_TO_CART
-            <button data-gated 
-                    data-gate="{$this->params->get('gate')}"
-                    class="{$this->params->get('class')}">
-                {$this->params->get('text', 'Add to cart')}
-            </button>
-        ADD_TO_CART;
+        return $this->renderButton(route('gated.cart.add'), 'Add to cart');
     }
 
-    public function removeFromCart() : string
+    public function removeFromCart(): string
     {
-        return <<<ADD_TO_CART
-            <button data-gated-remove 
-                    data-gate="{$this->params->get('gate')}"
-                    class="{$this->params->get('class')}">
-                {$this->params->get('text', 'Remove from cart')}
-            </button>
-        ADD_TO_CART;
+        return $this->renderButton(route('gated.cart.remove'), 'Remove from cart');
     }
 
+    private function renderButton(string $route, string $defaultText)
+    {
+        $csrfField = csrf_field();
+        $gate = $this->params->get('gate');
+        $buttonText = $this->params->get('text', $defaultText);
+        $class = $this->params->get('class');
+        $redirect = $this->params->get('redirect');
+
+        if (blank($gate)) {
+            throw new \Exception("Add to cart: gate param is required");
+        }
+
+        return <<<ADD_TO_CART
+            <form action="{$route}" method="post">
+                {$csrfField}
+                <input type="hidden" name="gate" value="{$gate}">
+                <input type="hidden" name="redirect" value="{$redirect}">
+                <button class="{$class}" type="submit">
+                    {$buttonText}
+                </button>
+            </form>
+        ADD_TO_CART;
+    }
 
     public function ownedGates()
     {
-        if(!Auth::check()) {
+        if (!Auth::check()) {
             return [];
         }
 
         $userGates = Auth::user()->get('gates', []);
 
         $userGates = array_map(function ($userGate) {
-                $userGate['gate'] = Gate::findBySlug($userGate['handle'])->toArray();
-                return $userGate;
+            $userGate['gate'] = Gate::findBySlug($userGate['handle'])->toArray();
+            return $userGate;
         }, $userGates);
 
         return $userGates;
