0707297f36b360a7533af8b378358b053101b18c
Christian <christianpavilonis@outlook.com>
2025-04-18 14:35:55 -0500

    Replace $request->user() with Statamic User Facade

--- src/Http/Controllers/Payment/CreatePaymentIntent.php
+++ src/Http/Controllers/Payment/CreatePaymentIntent.php
@@ -4,15 +4,14 @@
 namespace State\Walls\Http\Controllers\Payment;
 
 
-use Illuminate\Http\Request;
 use State\Walls\Payment\PaymentIntentFactory;
-use State\Walls\PaymentWall;
+use Statamic\Facades\User;
 
 class CreatePaymentIntent
 {
-    public function __invoke(Request $request, PaymentIntentFactory $intentFactory)
+    public function __invoke(PaymentIntentFactory $intentFactory)
     {
-        $intent = $intentFactory->user($request->user())->build();
+        $intent = $intentFactory->user(User::current())->build();
 
         return [
             'clientSecret' => $intent->client_secret,
